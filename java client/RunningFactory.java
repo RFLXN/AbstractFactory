88ce618b5de2a60d4d92a8b9c0681de068a6668c
jc19304024 <jc19304024@ga.tera-house.ac.jp>
2020-09-11 21:34:28 +0900

    no message

--- java client/RunningFactory.java
+++ java client/RunningFactory.java
@@ -3,7 +3,7 @@ import answersend.SendAnswersCommand;
 import questionfind.FindQuestionCommand;
 import questionfind.RunningFindQuestionCommand;
 
-public class RunningFactory extends QuestionFactory {
+public class RunningFactory extends QuestionFactory {       // 本番用のFactory
     @Override
     public FindQuestionCommand createFindingQuestion() {
         return new RunningFindQuestionCommand();
